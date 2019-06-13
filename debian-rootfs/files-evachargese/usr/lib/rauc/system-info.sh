8d254c8a992db5743c794084ae0f9f3cf2da9752
Michael Heimpold <michael.heimpold@in-tech.com>
2019-06-13 14:23:54 +0200

    rauc: try baptism data first when looking for a serial number
    Signed-off-by: Michael Heimpold <michael.heimpold@in-tech.com>

--- debian-rootfs/files-evachargese/usr/lib/rauc/system-info.sh
+++ debian-rootfs/files-evachargese/usr/lib/rauc/system-info.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-RAUC_SYSTEM_SERIAL=$(fw_printenv -n serial# 2>/dev/null)
+# obtain serial number from baptism data, otherwise look in normal U-Boot env
+RAUC_SYSTEM_SERIAL=$(fw_printenv -n serial# -c /etc/baptism-data.config 2>/dev/null)
+if [ -z "$RAUC_SYSTEM_SERIAL" ]; then
+	RAUC_SYSTEM_SERIAL=$(fw_printenv -n serial# 2>/dev/null)
+fi
+
+# pass serial to rauc if available
 if [ -n "$RAUC_SYSTEM_SERIAL" ]; then
 	echo RAUC_SYSTEM_SERIAL=$RAUC_SYSTEM_SERIAL
 fi
